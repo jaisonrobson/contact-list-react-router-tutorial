e98e64ccdeeb13318b56e8fb032c411ee9244f5e
jaisonrobson <xd_jrg@hotmail.com>
2024-07-23 02:42:15 -0300

    refactor: utilizado `NavLink` para evidenciar o contato selecionado da lista

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link, useLoaderData, Form, redirect } from "react-router-dom"
+import { Outlet, Link, useLoaderData, Form, redirect, NavLink } from "react-router-dom"
 import { getContacts, createContact } from "../contacts"
 
 export async function action() {
@@ -49,7 +49,16 @@ export default function Root() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
+                    <NavLink
+                      to={`contacts/${contact.id}`}
+                      className={({isActive, isPending }) =>
+                          isActive
+                          ? "active"
+                          : isPending
+                          ? "pending"
+                          : ""
+                      }
+                    >
                       {contact.first || contact.last ? (
                         <>
                           {contact.first} {contact.last}
@@ -59,7 +68,7 @@ export default function Root() {
                       )}
                       {" "}
                       {contact.favorite && <span>★</span>}
-                    </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
