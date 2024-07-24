145f3373dc0d05b6d82479315b58efc2fa1423f7
jaisonrobson <xd_jrg@hotmail.com>
2024-07-24 06:22:08 -0300

    feat: adicionado spinner de carregamento na barra de pesquisa

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -21,6 +21,8 @@ export default function Root() {
   const navigation = useNavigation()
   const submit = useSubmit()
 
+  const searching = navigation.location && new URLSearchParams(navigation.location.search).has("q")
+
   useEffect(() => {
     document.getElementById("q").value = q //manipulacao direta do DOM
   }, [q])
@@ -33,6 +35,7 @@ export default function Root() {
             <Form id="search-form" role="search">
               <input
                 id="q"
+                className={searching ? "loading" : ""}
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
@@ -43,7 +46,7 @@ export default function Root() {
               <div
                 id="search-spinner"
                 aria-hidden
-                hidden={true}
+                hidden={!searching}
               />
               <div
                 className="sr-only"
